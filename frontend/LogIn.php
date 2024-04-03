25b1b4baf5ef9fb19b6939e6a1e8427099b20d92
Punnychai <rpunnachai@gmail.com>
2024-04-03 21:17:19 +0700

    Implement JavaScript on LogIn

--- frontend/LogIn.php
+++ frontend/LogIn.php
@@ -5,19 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Style.css">
     <title>Log In</title>
-
 </head>
+
 <body>
     <div class="center">
-        <form action="" method="post" class="formInput">
+        <form action="LogIn.php" method="post" class="formInput" id="loginForm">
             <h1>Log In</h1>
             <div>
                 <img src="user.png" alt="User Icon">
-                <input type="text" name="username" id="username" placeholder="Username">
+                <input type="text" name="username" id="username" placeholder="Username" required>
             </div>
             <div>
                 <img src="lock.png" alt="Lock Icon">
-                <input type="password" name="passwd" id="passwd" placeholder="Password">
+                <input type="password" name="pwd" id="pwd" placeholder="Password" required>
             </div>
             <input type="submit" name="LogIn" value="LOG IN">
             <p class="switchPage">
@@ -25,4 +25,25 @@
             </p>
         </form>
     </div>
+    <p style="display: flex; justify-content: center; align-items: center; margin: -20vh 0 0 0; padding: 0;" id="loginMessage"></p>
+
+    <script>
+        document.getElementById("loginForm").addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("pwd").value;
+
+            //dummy credentials
+            var validUser = "admin";
+            var validPass = "admin1234";
+
+            if (username === validUser && password === validPass) {
+                window.location.href = "https://www.siit.tu.ac.th";
+            }
+            else {
+                document.getElementById("loginMessage").innerText = "Invalid Username or Password. Please try again.";
+            }
+        });
+    </script>
 </body>
--- frontend/SignUp.php
+++ frontend/SignUp.php
@@ -9,23 +9,23 @@
 
 <body>
     <div class="center">
-        <form action="" method="post"class="formInput">
+        <form action="" method="post" class="formInput" id="signupForm">
             <h1>Sign Up</h1>
             <div>
                 <img src="user.png" alt="User Icon">
-                <input type="text" name="username" id="username" placeholder="Username">
+                <input type="text" name="username" id="username" placeholder="Username" required>
             </div>
             <div>
                 <img src="mail.png" alt="Mail Icon">
-                <input type="text" name="email" id="email" placeholder="Email">
+                <input type="text" name="email" id="email" placeholder="Email" required>
             </div>
             <div>
                 <img src="lock.png" alt="Lock Icon">
-                <input type="password" name="pwd" id="pwd" placeholder="Password">
+                <input type="password" name="pwd" id="pwd" placeholder="Password" required>
             </div>
             <div>
                 <img src="lock.png" alt="Lock Icon">
-                <input type="password" name="cfpwd" id="cfpwd" placeholder="Confirm Password">
+                <input type="password" name="cfpwd" id="cfpwd" placeholder="Confirm Password" required>
             </div>
             <input type="submit" name="SignUp" value="REGISTER NOW">
             <p class="switchPage">Already have an account?
